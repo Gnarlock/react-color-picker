1b3afa48b204d73f9d9cdf19edf7adad71cabc72
Adam Perez <adam.perez@steelseries.com>
2017-06-14 00:48:23 -0500

    Don't use this stuff ever since I Started using Color library

--- src/utils/color.js
+++ src/utils/color.js
@@ -1,30 +1,7 @@
-const channels = ["r","g","b"];
-function getIndexOfChannel(channel) {
-  return channels.indexOf(channel);
-}
-
 const rgbDecimalRegex = RegExp('^[0-9]{0,3}$', 'i');
 const rgbHexRegex = RegExp('^#[0-9A-F]{0,6}$', 'i');
 
 module.exports = {
-  rgb: {
-    channels: {
-      array: channels,
-      getIndex: getIndexOfChannel,
-      names: {
-        short: {
-          red: "r",
-          green: "g",
-          blue: "b"
-        },
-        long: {
-          r: "red",
-          g: "green",
-          b: "blue"
-        }
-      }
-    }
-  },
   rgbDecimalRegex: rgbDecimalRegex,
   rgbHexRegex: rgbHexRegex
 }
