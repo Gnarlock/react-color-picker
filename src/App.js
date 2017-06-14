0b32caf081670e352dc8462e4bf47200d0d05921
Adam Perez <adam.perez@steelseries.com>
2017-06-14 16:18:19 -0500

    Don't re-render HSL if color hasn't changed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <ColorPicker />
+        <ColorPicker displayHSL={true} displayRGB={true} />
       </div>
     );
   }
