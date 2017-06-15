0357add3f3c9affa50e26b2322310f4e1f5240f3
Adam Perez <adam.perez@steelseries.com>
2017-06-15 01:13:04 -0500

    1 more color update :D

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import React, { Component } from 'react';
 import './styles/App.css';
 import ColorPicker from './widgets/ColorPicker/ColorPicker'
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
-      	<div className="TopControlBar">
-      		<span>Color Picker</span>
-          <button>x</button>
-      	</div>
+        { this.props.displayControls && 
+            <div className="TopControlBar">
+              <span>Color Picker</span>
+              <button>x</button>
+            </div>
+        }
 
         <ColorPicker displayHSL={true} displayRGB={true} />
 
-        <div className="BottomControlBar" >
-        	<button>Cancel</button>
-          <button>Submit</button>
-        </div>
+        { this.props.displayControls &&
+            <div className="BottomControlBar" >
+              <button>Cancel</button>
+              <button>Submit</button>
+            </div>
+        }
       </div>
     );
   }
 }
 
-export default App;
+App.defaultProps = {
+  displayControls: true
+};
