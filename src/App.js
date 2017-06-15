13c255a5836209717b386f4a08e522b82b779bde
Adam Perez <adam.perez@steelseries.com>
2017-06-15 00:52:32 -0500

    Probably the last UI update - add non functioning buttons and things

--- src/App.js
+++ src/App.js
@@ -6,9 +6,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      	<div className="TopControlBar" />
+      	<div className="TopControlBar">
+      		<span>Color Picker</span>
+          <button>x</button>
+      	</div>
+
         <ColorPicker displayHSL={true} displayRGB={true} />
-        <div className="BottomControlBar" />
+
+        <div className="BottomControlBar" >
+        	<button>Cancel</button>
+          <button>Submit</button>
+        </div>
       </div>
     );
   }
